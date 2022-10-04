aa443358dfcc843067a0ba38fe4fa10aaf5b7bd3
Renata Klepek <klepekrenata@gmail.com>
2022-10-04 19:56:58 +0200

    Added stop function

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let alarm = new Audio('img/alarm.mp3');
 let timerStarted = false;
+let timerInterval;
+let stopInterval;
 
 function startTimer() {
     if (!timerStarted) {
         let startTime = new Date().getTime();
-        let fiveMinutes = 1000 * 5;
-        //let fiveMinutes = 1000 * 60 * 5;
+        let fiveMinutes = 1000 * 60 * 5;
+        //let fiveMinutes = 1000 * 5;
         let endTime = startTime + fiveMinutes;
 
-        setInterval(function() {
+        timerInterval = setInterval(function() {
             let timeLeft = endTime - new Date().getTime();
+            timerStarted = true;
 
             if (timeLeft > 0) {
                 let minutes = timeLeft / (1000 * 60);
@@ -22,8 +25,17 @@ function startTimer() {
             } else {
                 alarm.play();
                 timer.innerHTML = '00 : 00';
+                timerStarted = false;
+
+                stopInterval = setInterval(stopAlarm, 1000 * 5);
             }
         }, 1000);
-        timerStarted = true;
     }
+}
+
+function stopAlarm() {
+    clearInterval(timerInterval);
+    clearInterval(stopInterval);
+    alarm.pause();
+    timer.innerHTML = '05 : 00';
 }
